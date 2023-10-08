c430def4c71f4555e9d2a19974e3b04a43fa23d1
neesoh <34525424+neesoh@users.noreply.github.com>
2023-10-07 22:49:54 -0700

    Input Validation
    Started Input Validation. Fixed errors and left comments for other ones.

--- inputs.py
+++ inputs.py
@@ -1,5 +1,33 @@
 import argparse
 import json
+import sys
+
+def user_prompt(x, i):
+    answer = 0
+    while ((answer != " Yes") & (answer != "No")):
+        if (i <= 1):
+            answer = input("Do you want to change anything?")
+        else: 
+            answer = input("Do you want to change anything else?")
+        if (answer == " Yes"):
+            i = i + 1
+            answer2 = 0
+            while ((answer2 != " Name") & (answer2 != " Substitute Method") & (answer2 != " Substitute Assignment")):
+                answer2 = input("What do you want to change?")
+                if (answer2 == " Name"):
+                    answer3 = input("What would you like the new name to be?")
+                    x['Name'] = answer3 #This part isn't working because the name isn't actually being changed. 
+                elif (answer2 == " Substitute Method"):
+                    answer3 = 0
+                    while ((answer3 != " average") & (answer3 != " weighted_average") & (answer3 != " max") & (answer3 != " min") & (answer3 != " median")):
+                        answer3 = input("What would you like the new substitute method to be?")
+                    x['Substitute Method'] = answer3 #This part isn't working because the substitute method isn't actually being changed.
+                elif (answer2 == " Substitute Assignments"):
+                    #Work on this part later. I need to ask how many assignments there are and accept that number of inputs. 
+                    answer3 = input("What would you like the new substitute assignments to be?")
+                    x['Substitute Assignment'] = answer3 #This part isn't working because the substitute assignments aren't actually being changed.
+        elif (answer == " No"):
+            sys.exit()
 
 class EnvironmentSettings:
     def __init__(self):
@@ -24,6 +52,17 @@ class HomeworkManager:
             "Substitute Assignments": substitute_assignments
         }
         self.homework_list.append(homework_data)
+
+    def print_homework_data(self):
+        i = 0
+        print("\nHomework List:")
+        for homework in self.homework_list:
+            print(f"Name: {homework['Name']}") # This part isn't working. It keeps printing the wrong name. 
+            # print(f"ID: {homework['ID']}")
+            print(f"Substitute Method: {homework['Substitute Method']}") # This line isn't working. It keeps saying the substitute method is "average" regardless of what I put in. 
+            print(f"Substitute Assignments: {homework['Substitute Assignments']}") # This line isn't working. It keeps printing "Test Quiz", "Test Quiz 2", and "Test Quiz 3" regardless of what I put in. 
+            i = i + 1
+            user_prompt(homework, i)
 
 def save_data(environment, homework_manager):
     data = {
@@ -88,8 +127,11 @@ if __name__ == "__main__":
     if args.add_homework:
         # Prompt the user for homework details
         name = input("Enter the name of the homework: ")
-        substitute_method = input("Enter the substitute method (average, weighted_average, max, min, median): ")
-        number_of_sub_assignments = input("Enter the number of assignments you want to use to calculate substitute grade: ")
+        substitute_method = 0
+        while ((substitute_method != "average") & (substitute_method != "weighted_average") & (substitute_method != "max") & (substitute_method != "min") & (substitute_method != "median")):
+            substitute_method = input("Enter the substitute method (average, weighted_average, max, min, median): ")
+            if ((substitute_method == "average") | (substitute_method == "weighted_average") | (substitute_method == "max") | (substitute_method == "min") | (substitute_method == "median")):
+                number_of_sub_assignments = input("Enter the number of assignments you want to use to calculate substitute grade: ")
 
         substitute_assignments = get_substitute_assignments(number_of_sub_assignments)
         
@@ -106,11 +148,5 @@ if __name__ == "__main__":
     print(f"Setting1: {environment.setting1}")
     print(f"Setting2: {environment.setting2}")
 
-    print("\nHomework List:")
-    for homework in homework_manager.homework_list:
-        print(f"Name: {homework['Name']}")
-        # print(f"ID: {homework['ID']}")
-        print(f"Substitute Method: {homework['Substitute Method']}")
-        print(f"Substitute Assignments: {homework['Substitute Assignments']}")
-
+    homework_manager.print_homework_data()
 
