16a005064413f0fc40fbfee4e9393b7ee7924d19
avgale <avgale@ucdavis.edu>
2023-10-08 14:31:18 -0700

    Parsed assignment input to allow misspelling in the name but not the number.

--- input.py
+++ input.py
@@ -1,5 +1,6 @@
 from exit import check_if_exiting
 from search import search
+from parse_hw_name import split_name_num
 
 def get_command():
     command = input("Enter a command: ")
@@ -16,9 +17,28 @@ def get_assignment(course):
 
     check_if_exiting(user_input.lower())
 
+    # Separate assignment name and number to allow misspellings in name but no error in number
+    input_name, input_num = split_name_num(user_input)
+
     assignments = course.get_assignments()
     assignment_names = [(assignment.name).lower() for assignment in assignments]
-    assignment_name = search(assignment_names, user_input.lower(), 80)
+
+    # Separate existing assignment names into name and number to compar to input name and number
+    target_names = []
+    target_nums = []
+    for name in assignment_names:
+        target_name, target_num = split_name_num(name)
+        target_names.append(target_name)
+        target_nums.append(target_num)
+
+    valid_name = search(target_names, input_name, 80)
+    if (input_num != []):
+        valid_num = search(target_nums, input_num, 100)
+    
+    if (input_name and not valid_name) or (input_num and not valid_num):
+        return None
+
+    assignment_name = valid_name + valid_num
 
     return assignment_name
 
