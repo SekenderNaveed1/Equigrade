515eedc59ee13c53eb145bd6dfd50ac6b1ccaf42
avgale <avgale@ucdavis.edu>
2023-10-08 18:39:16 -0700

    Average working for cases where all relevant assignments have submissions.

--- avg.py
+++ avg.py
@@ -11,23 +11,9 @@ def avg(course, assignment_name):
     grade_map = {}
     submission_scores = [0] * (total + 1)
 
-    for submission in submissions:
-        curr_student = submission.user_id
-        if submission.workflow_state=="unsubmitted":
-            continue
-        submission_scores[0] = submission.score
-        grade_map[curr_student] = submission_scores
+    create_map(course, grade_map, submissions, submission_scores, substitute_assignments)
     
-    index = 1
-    for sub_assignment in substitute_assignments:
-        assignment = get_assignment_data(course, sub_assignment)
-        sub_submissions = assignment.get_submissions()
-        for sub_submission in sub_submissions:
-            if sub_submission.user_id in grade_map:
-                grade_map[sub_submission.user_id][index] = sub_submission.score
-        index+=1
-
-    print(grade_map)
+    update_grade(grade_map, total, submissions)
 
     #students = course.get_users()
     #for student in students:
@@ -66,27 +52,33 @@ def get_sub_assignments(course, total):
     
     return substitute_assignments
 
-def update_grade(student, course, assignment_name, substitute_assignments):
-    assignment = get_assignment_data(course, assignment_name)
-    student_id = student.sis_user_id
-    try:
-        submission = assignment.get_submission(student_id)
-    except:
-        print("No submission found.")
-        return
+def update_grade(grade_map, total, submissions):
+    for student in grade_map:
+        sum = 0
+        for i in range(1, len(grade_map[student])):
+            sum+=grade_map[student][i]
+        grade_map[student][0] = sum / total
     
-    sum = 0
-    for assignment in substitute_assignments:
-        assignment = get_assignment_data(course, assignment)
-        try:
-            sub_submission = assignment.get_submission(student_id)
-        except:
-            print("No substitute submission found.")
-        
-        if sub_submission.score is not None:
-            sum+=sub_submission.score
+    for submission in submissions:
+        if submission.user_id in grade_map:
+            submission.edit(submission={'posted_grade':grade_map[submission.user_id][0]})
 
-    new_score = sum / len(substitute_assignments)
+def create_map(course, grade_map, submissions, submission_scores, substitute_assignments):
+    for submission in submissions:
+        curr_student = submission.user_id
+        if submission.workflow_state=="unsubmitted":
+            continue
+        submission_scores[0] = submission.score
+        grade_map[curr_student] = submission_scores
+    
+    index = 1
+    for sub_assignment in substitute_assignments:
+        assignment = get_assignment_data(course, sub_assignment)
+        sub_submissions = assignment.get_submissions()
+        for sub_submission in sub_submissions:
+            if sub_submission.user_id in grade_map:
+                grade_map[sub_submission.user_id][index] = sub_submission.score
+        index+=1
 
-    submission.edit(submission={'posted_grade':new_score})
+    
 
--- substitutions.py
+++ substitutions.py
@@ -3,24 +3,15 @@ from sub_weighted_avg import weighted_avg
 
 def substitute(sub_method, course, homework_manager, assignment_name):
     if sub_method == "average":
-        new_score, sub_assignments = avg(course, assignment_name)
+        sub_assignments = avg(course, assignment_name)
 
     elif sub_method == "weighted_average":
-        new_score, sub_assignments = weighted_avg(course)
+        sub_assignments = weighted_avg(course)
 
     else:
-        avg_score, sub_assignments_avg = avg(course)
-        weighted_avg_score, sub_assignments_weighted_avg = weighted_avg(course)
-
-        new_score = max(avg_score, weighted_avg_score)
-
-        if (new_score == avg_score): # Need to change this to account for possibility of avg_score and weighted_avg_score being the same
-            sub_assignments = sub_assignments_avg
-        else:
-            sub_assignments = sub_assignments_weighted_avg
+        max()
     
     substitute_assignments = sub_assignments
     homework_manager.add_homework(assignment_name, sub_method, substitute_assignments)
-    
-    return new_score
 
+    print("The assignment has been updated.")
