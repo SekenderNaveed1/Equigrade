dd19c169ddb8c4e3949610891bf2a866cbdf1fa6
AlexGarcia <129991936+AlexGarcia508@users.noreply.github.com>
2023-10-17 21:54:09 -0700

    check description
    -changed how data in json files are handled
    -is able to call sub_weighted_avg function
    -still needs work to be able to pull the right parameters/values for the sub_weighted_avg function

--- inputs.py
+++ inputs.py
@@ -1,45 +1,38 @@
 import argparse
 import json
-
+from sub_weighted_avg import sub_weighted_avg
 
 class EnvironmentSettings:
     def __init__(self):
         self.setting1 = None
-        self.setting2 = None
 
     def set_setting(self, setting_name, value):
         if setting_name == 'setting1':
             self.setting1 = value
-        elif setting_name == 'setting2':
-            self.setting2 = value
-
 
 class HomeworkManager:
     def __init__(self):
         self.homework_list = []
 
-    def add_homework(self, name, substitute_method, substitute_assignments):
+    def add_homework(self, name, score, weight):
         homework_data = {
             "Name": name,
             # "ID": id,
-            "Substitute Method": substitute_method,
-            "Substitute Assignments": substitute_assignments
+            "Score": score,
+            "Weight": weight
         }
         self.homework_list.append(homework_data)
-
 
 def save_data(environment, homework_manager):
     data = {
         "environment": {
-            "setting1": environment.setting1,
-            "setting2": environment.setting2
+            "setting1": environment.setting1
         },
         "homework_list": homework_manager.homework_list
     }
 
     with open("data.json", "w") as data_file:
         json.dump(data, data_file)
-
 
 def load_data():
     try:
@@ -52,21 +45,12 @@ def load_data():
         }
     return data
 
-
-def get_substitute_assignments(number_of_substitute_assignments):
-    substitute_assignments = []
-    for i in range(int(number_of_sub_assignments)):
-        substitute_assignments.append(input(f"Enter substitute assignment {i + 1}: "))
-    return substitute_assignments
-
-
 if __name__ == "__main__":
     data = load_data()
 
     # Initialize EnvironmentSettings and HomeworkManager objects with loaded data
     environment = EnvironmentSettings()
     environment.setting1 = data["environment"].get("setting1")
-    environment.setting2 = data["environment"].get("setting2")
 
     homework_manager = HomeworkManager()
     homework_manager.homework_list = data["homework_list"]
@@ -76,7 +60,6 @@ if __name__ == "__main__":
 
     # Define command-line flags
     parser.add_argument("--setting1", help="Set the value of setting1")
-    parser.add_argument("--setting2", help="Set the value of setting2")
     parser.add_argument("--add-homework", action="store_true", help="Add homework")
 
     # Parse command-line arguments
@@ -86,21 +69,28 @@ if __name__ == "__main__":
     if args.setting1:
         environment.set_setting("setting1", args.setting1)
 
-    if args.setting2:
-        environment.set_setting("setting2", args.setting2)
+    if args.setting1.lower() == "weighted average":
+        # Read numbers from JSON file
+        with open('data.json') as json_file:
+            data = json.load(json_file)
+            weight1 = 40 #int(data["Weight1"])
+            weight2 = 60 #int(data["Weight2"])
+            score1 = 85 #int(data["Score1"])
+            score2 = 90 #int(data["Score2"])
+
+    percent = sub_weighted_avg(score1, score2, weight1, weight2)
+
 
     # Perform actions based on command-line flags
     if args.add_homework:
         # Prompt the user for homework details
         name = input("Enter the name of the homework: ")
-        substitute_method = input("Enter the substitute method (average, weighted_average, max, min, median): ")
-        number_of_sub_assignments = input(
-            "Enter the number of assignments you want to use to calculate substitute grade: ")
-
-        substitute_assignments = get_substitute_assignments(number_of_sub_assignments)
+        #substitute_method = input("Enter the substitute method (average, weighted_average, max, min, median): ")
+        score = input("Enter the score of the homework: ")
+        weight = input("Enter the weight of the homework: ")
 
         # Add the homework with the provided details
-        homework_manager.add_homework(name, substitute_method, substitute_assignments)
+        homework_manager.add_homework(name, score, weight)
 
         # Save data immediately after adding a homework
         save_data(environment, homework_manager)
@@ -108,11 +98,11 @@ if __name__ == "__main__":
     # Print or manipulate the environment and homework data as needed
     print("Environment Settings:")
     print(f"Setting1: {environment.setting1}")
-    print(f"Setting2: {environment.setting2}")
 
     print("\nHomework List:")
     for homework in homework_manager.homework_list:
         print(f"Name: {homework['Name']}")
         # print(f"ID: {homework['ID']}")
-        print(f"Substitute Method: {homework['Substitute Method']}")
-        print(f"Substitute Assignment: {homework['Substitute Assignment']}")
+        print(f"Score: {homework['Score']}")
+        print(f"Weight: {homework['Weight']}")
+    print(percent)
